a7387703def2ceea1d1d87f69c5b8264922d929f
minjee <kimmj1106@naver.com>
2022-01-30 01:01:26 +0900

    Spring 패턴으로 yoilteller 구현, gitignore추가

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,13 +1,10 @@
 package com.fastcampus.ch2;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //년월일을 입력하면 요일을 알려주는 프로그램을 만들어보자.
@@ -16,27 +13,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class YoilTellerMVC {
 	
 	@RequestMapping("/getYoilMVC")
-	public void main(int year, int month, int day, HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public String main(int year, int month, int day, Model model) throws IOException {
 
+		//1. 유효성 검사
+		if(!isValid(year, month, day)) {
+			return "yoilError"; //년, 월, 일이 유효하지 않으면 WEB-INF/views/yoilError.jsp를 보여주겠다!
+		}
+		
+		//2. 요일 계산
+		char yoil = getYoil(year, month, day);
+		
+		//3. 계산한 결과를 model에 저장
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("yoil", yoil);
+		
+		return "yoil"; // /WEB-INF/views/yoil.jsp를 이용해서 결과물을 보여주겠다 라는 뜻!
+					   // 근데 경로와 확장자 부분은 매번 똑같으니까 공통된 부분은 빼고 view이름만 적도록 하는 것!
+		
+	}
+
+	private boolean isValid(int year, int month, int day) {
+		return true;
+	}
+
+	private char getYoil(int year, int month, int day) {
 		Calendar cal = Calendar.getInstance();
 		cal.set(year, month - 1, day);
 		
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); //숫자로 출력됨. 1=일, 2=월, 3=화 ...
-		char yoil = "일월화수목금토".charAt(dayOfWeek); //숫자를 다시 문자로 바꿔준다.
-		
-		//3-1. 콘솔에 출력
-//		System.out.println(year + "년 " + month + "월 " + day + "일은 ");
-//		System.out.println(yoil + "요일 입니다.");
-		
-		//3-2. 브라우저에 출력
-//		response.setContentType("text/html"); //출력할 내용의 타입을 써주어야 한다. why?브라우저는 내가 보내는 내용이 텍스트인지 바이너리인지 모르기 때문에 알려줘야한다.
-//		response.setCharacterEncoding("utf-8"); //인코딩도 알려줘야 함
-//		PrintWriter out = response.getWriter(); //response객체에서 브라우저로의 출력 스트림을 얻는다.
-//												//response.getWriter()는 예외 처리를 해주어야 하는데
-//												//지금은 일단 던지겠다.
-//		
-//		out.println(year + "년 " + month + "월 " + day + "일은 ");
-//		out.println(yoil + "요일 입니다.");
+		return "일월화수목금토".charAt(dayOfWeek); //숫자를 다시 문자로 바꿔준다.
 	}
 
 }
