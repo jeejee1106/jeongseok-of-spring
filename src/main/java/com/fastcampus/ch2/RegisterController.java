0ed0974ad27d7a076e3926730fe7bf4a57dd5269
minjee <kimmj1106@naver.com>
2022-02-06 16:40:10 +0900

    @GetMappting, @PostMapping 학습

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,18 +1,48 @@
 package com.fastcampus.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
 
-	@RequestMapping("/register/add")
-	public String register() {
-		return "registerForm";
+//	@RequestMapping("/register/add") //(value="/register/add", method={RequestMethod.GET, RequestMethod.POST}) 이 생략되어 있는 것! GET과 POST 둘 다 허용하겠다.
+//	@GetMapping("/register/add")
+//	public String register() {
+//		return "registerForm";
+//	}
+	/*
+	 * 사실 위 메서드는 매핑 연결만 해줄 뿐 별다른 기능이 없는 메서드이다.
+	 * 이런 메서드들은 servlet-context.xml파일에 
+	 * <view-controller path="/register/add" view-name="registerForm"/>
+	 * 이렇게 한 줄을 추가해주면 위 네줄을 안쓰고도 같은 기능을 하게 만들 수 있다.
+	 * view-controller는 get요청만 허락한다!
+	 * */
+	
+
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST) //아래와 동일한 코드!
+	@PostMapping("/register/save") //PostMapping, GetMapping 어노태이션은 스프링 4.3부터!!
+	public String save(User user, Model m) throws Exception{
+		//1. 유효성 검사
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+			
+//			return "redirect:/register/add?msg=" + msg; //URL재작성(rewriting)
+			
+			//위 msg를 모델로 보내는 방법도 있다.
+			m.addAttribute("msg", msg);
+			return "redirect:/register/add";
+		}
+		
+		return "registerInfo";
 	}
 
-	@RequestMapping("/register/save")
-	public String save() {
-		return "registerInfo";
+	private boolean isValid(User user) {
+		return true;
 	}
 }
--- src/main/webapp/WEB-INF/views/registerInfo.jsp
+++ src/main/webapp/WEB-INF/views/registerInfo.jsp
@@ -6,15 +6,16 @@
 		<title>Insert title here</title>
 	</head>
 	<body>
-		<h1>id=${param.id}</h1>
-		<h1>pwd=${param.pwd}</h1>
-		<h1>name=${param.name}</h1>
-		<h1>email=${param.email}</h1>
-		<h1>birth=${param.birth}</h1>
+		<h1>id=${user.id}</h1>
+		<h1>pwd=${user.pwd}</h1>
+		<h1>name=${user.name}</h1>
+		<h1>email=${user.email}</h1>
+		<h1>birth=${user.birth}</h1>
+		<h1>sns=${user.sns}</h1>
 		<!-- registerForm.html에서 보내는 name값이 모두 sns이다. 즉 같은 name값이 여러개이다. -->
 		<!-- 그럴 떈 paramValues라는 배열에 담아서 출력해주어야 한다. -->
-		<h1>sns=${paramValues.sns[0]}</h1>
+		<!-- <h1>sns=${paramValues.sns[0]}</h1>
 		<h1>sns=${paramValues.sns[1]}</h1>
-		<h1>sns=${paramValues.sns[2]}</h1>
+		<h1>sns=${paramValues.sns[2]}</h1>  -->
 	</body>
 </html>
