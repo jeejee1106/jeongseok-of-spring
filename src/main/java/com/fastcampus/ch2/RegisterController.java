0e0f9fddcf1b45cf3e9e47ec2c85674707a03a2f
minjee <kimmj1106@naver.com>
2022-02-10 18:27:48 +0900

    Validator 학습

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,16 +1,15 @@
 package com.fastcampus.ch2;
 
-import java.net.URLEncoder;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
+import javax.validation.Valid;
+
 import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +23,14 @@ public class RegisterController {
 	public void toDate(WebDataBinder binder) {
 //		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 //		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+		
 		binder.registerCustomEditor(String[].class, new StringArrayPropertyEditor("#")); //스프링이 제공하는 기능. 문자열 잘라서 배열에 넣기
+		
+		binder.setValidator(new UserValidator()); //UserValidator를 WebDataBinder의 로컬 validator로 등록 - 자동 검증을 위해??
+		
+//		binder.addValidators(new UserValidator());
+		List<Validator> validatorList =  binder.getValidators();
+		System.out.println("validatorList = " + validatorList);
 	}
 
 //	@RequestMapping("/register/add") //(value="/register/add", method={RequestMethod.GET, RequestMethod.POST}) 이 생략되어 있는 것! GET과 POST 둘 다 허용하겠다. 아래와 동일한 코드
@@ -45,19 +51,30 @@ public class RegisterController {
 
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST) //아래와 동일한 코드!
 	@PostMapping("/save") //PostMapping, GetMapping 어노태이션은 스프링 4.3부터!!
-	public String save(User user, BindingResult result, Model m) throws Exception{
+	public String save(@Valid User user, BindingResult result, Model m) throws Exception{
 		System.out.println("result = " + result);
 		System.out.println("user = " + user);
-		//1. 유효성 검사
-		if(!isValid(user)) {
-			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
-			
-//			return "redirect:/register/add?msg=" + msg; //URL재작성(rewriting)
-			
-			//위 msg를 모델로 보내는 방법도 있다.
-			m.addAttribute("msg", msg);
-			return "forward:/register/add";
+		
+		//user객체에 올바른 타입이 들어갔나 검증하는건가...?...?
+		//수동 검증 - Validator를 직접 생성하고, validate()를 직접 호출
+//		UserValidator userValidator = new UserValidator();
+//		userValidator.validate(user, result); //BindingResult는 Errors의 자손
+		
+		//User객체를 검증한 결과 에러가 있으면 registerForm을 이용해서 에러를 보여줘야함
+		if(result.hasErrors()) {
+			return "registerForm";
 		}
+		
+//		//1. 유효성 검사
+//		if(!isValid(user)) {
+//			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+//			
+////			return "redirect:/register/add?msg=" + msg; //URL재작성(rewriting)
+//			
+//			//위 msg를 모델로 보내는 방법도 있다.
+//			m.addAttribute("msg", msg);
+//			return "forward:/register/add";
+//		}
 		
 		return "registerInfo";
 	}
